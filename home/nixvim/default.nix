ea07b68298a363ef6b860500dbb7ed5367c73494
hrotovb001 <bohdanhrotovytskyy@gmail.com>
2024-10-13 16:22:23 -0400

    updated my nixvim config to include lsp and syntax highlighting

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -5,7 +5,10 @@
     enable = true;
     colorschemes.tokyonight = {
       enable = true;
-      settings.transparent = true;
+      settings = {
+        transparent = true;
+        on_colors = ''function (colors) colors.fg_gutter = "#b2b8cf" end'';
+      };
     };
     opts = {
       tabstop = 4;
@@ -18,15 +21,45 @@
     globals.mapleader = " ";
     keymaps = [
       { mode = "n"; key = "<leader>pv"; action = ":Ex<CR>"; options = { noremap = true; }; }
-      { mode = "n"; key = "<leader>ff"; action = ":Telescope find_files<CR>"; options = { noremap = true; }; }
-      { mode = "n"; key = "<leader>fg"; action = ":Telescope live_grep<CR>"; options = { noremap = true; }; }
-      { mode = "n"; key = "<leader>fb"; action = ":Telescope buffers<CR>"; options = { noremap = true; }; }
-      { mode = "n"; key = "<leader>fr"; action = ":Telescope resume<CR>"; options = { noremap = true; }; }
     ];
     plugins = {
       codeium-vim.enable = true;
       web-devicons.enable = true;
-      telescope.enable = true;
+      telescope = {
+        enable = true;
+        keymaps = {
+          "<leader>ff" = "find_files";
+          "<leader>fg" = "live_grep";
+          "<leader>fb" = "buffers";
+          "<leader>fr" = "resume";
+        };
+      };
+      lsp = {
+        enable = true;
+        servers = {
+            jedi_language_server.enable = true;
+        };
+        keymaps.lspBuf = {
+          K = "hover";
+          gD = "references";
+          gd = "definition";
+          gi = "implementation";
+          gt = "type_definition";
+        };
+      };
+      treesitter = {
+        enable = true;
+        settings.highlight.enable = true;
+      };
+      cmp = {
+        enable = true;
+        autoEnableSources = true;
+        settings.sources = [
+          { name = "nvim_lsp"; }
+          { name = "path"; }
+          { name = "buffer"; }
+        ];
+      };
     };
   };
 }
